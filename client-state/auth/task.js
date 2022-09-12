0bf10172b50e48e04f7cf01f87bb07f58b297174
Mechapa <mechapamachito@gmail.com>
2022-09-12 23:18:39 +0300

    client state fix

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,30 +1,41 @@
+const signin = document.getElementById('signin');
 const form = document.getElementById('signin__form');
-const singButton =document.getElementById('signin__btn');
-const welcome = document.querySelector('.welcome');
+const singButton = document.getElementById('signin__btn');
+const welcome = document.getElementById('welcome');
+const userIdValue = document.getElementById('user_id');
 
-singButton.addEventListener('click',function () {
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const formData = new FormData(form);
-        const xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', () =>{
-        
-        if(xhr.readyState === xhr.DONE){
-            response = JSON.parse(xhr.responseText);
-            if(response.success){
-                localStorage.setItem('user_id', response.user_id);
-                welcome.textContent = welcome.textContent + localStorage.getItem('user_id');
-                welcome.classList.add('welcome_active');
-            }
-            else{
-                alert("Неверный логин/пароль"); 
-            }
-        }
-        else {
-            alert("Ошибка " + xhr.status);
-        }
-        })
-        xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-        xhr.send(formData);
-    });
-})
+LogInUser();
+
+singButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () =>{
+    response = (xhr.response);
+    if (response.success) {
+      logInSucces(response.user_id);
+      localStorage.setItem('userId', response.user_id);
+      form.reset();
+    } else {
+      form.login.value = '';
+      form.password.value = '';
+      form.reset();
+      alert('Неверный логин/пароль');
+    };
+  });
+  xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+  xhr.send(formData);
+});
+
+function logInSucces(userId) {
+  signin.classList.remove('signin_active');
+  welcome.classList.add('welcome_active');
+  userIdValue.innerText = userId;
+};
+
+function LogInUser() {
+  if (localStorage.getItem('userId')) {
+    logInSucces(localStorage.getItem('userId'))
+  }
+};
